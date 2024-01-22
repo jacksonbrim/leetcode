d84022b80dced5c29d6aaff1c82cedcf528e132f
Jackson Brim <jackson.cr.brim@gmail.com>
2024-01-22 00:48:02 -0600

    Finish median sorted array, longest substring, and basic palindromic substring

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod add_two_nums;
 mod longest_substring;
 mod median_sorted_arrays;
+mod palindromic_substring;
 mod two_sum;
 
 fn main() {
--- src/median_sorted_arrays.rs
+++ src/median_sorted_arrays.rs
@@ -1,14 +1,25 @@
 pub fn find_median_sorted_arrays(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
-    let arr_len1 = nums1.len();
-    let arr_len2 = nums2.len();
-    let med_loc = arr_len1 + arr_len2 / 2;
+    let total_length = nums1.len() + nums2.len();
+    let mut p1 = 0;
+    let mut p2 = 0;
+    let mut last_val = 0;
+    let mut current_val = 0;
 
-    let mut med1: f64 = 0.;
-    let mut med2: f64 = 0.;
-    let mut med: f64 = 0.;
-    if (arr_len1 + arr_len2) % 2 == 0 {
-        let med_loc = (arr_len1 + arr_len2 / 2) - 1;
+    for _ in 0..=(total_length / 2) {
+        last_val = current_val;
+        if p1 < nums1.len() && (p2 >= nums2.len() || nums1[p1] <= nums2[p2]) {
+            current_val = nums1[p1];
+            p1 += 1;
+        } else {
+            current_val = nums2[p2];
+            p2 += 1;
+        }
+    }
+
+    if total_length % 2 == 0 {
+        (current_val + last_val) as f64 / 2.0
     } else {
+        current_val as f64
     }
 }
 
@@ -17,10 +28,30 @@ mod tests {
     use crate::median_sorted_arrays::*;
 
     #[test]
-    fn success() {
+    fn success1() {
         let nums1: Vec<i32> = vec![1, 3];
         let nums2: Vec<i32> = vec![2];
         let output1: f64 = 2.;
+
+        let res1 = find_median_sorted_arrays(nums1, nums2);
+
+        assert_eq!(res1, output1);
+    }
+    #[test]
+    fn success2() {
+        let nums1: Vec<i32> = vec![1, 2];
+        let nums2: Vec<i32> = vec![3, 4];
+        let output1: f64 = 2.5;
+
+        let res1 = find_median_sorted_arrays(nums1, nums2);
+
+        assert_eq!(res1, output1);
+    }
+    #[test]
+    fn success3() {
+        let nums1: Vec<i32> = vec![1, 2, 3, 3, 3, 4, 5, 8, 9, 12];
+        let nums2: Vec<i32> = vec![2, 2, 2, 2, 3, 4, 4, 8, 9, 12];
+        let output1: f64 = 3.5;
 
         let res1 = find_median_sorted_arrays(nums1, nums2);
 
