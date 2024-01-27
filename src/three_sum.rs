d0d7c8cd8f806173dadde3b4d93be4fb82249d86
Jackson Brim <jackson.cr.brim@gmail.com>
2024-01-26 21:46:17 -0600

    Three sum works, bad perf, though

--- src/three_sum.rs
+++ src/three_sum.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::HashSet;
 struct Solution;
 
 impl Solution {
@@ -12,22 +12,51 @@ impl Solution {
 
         let n = nums.len();
 
-        // Calculate the result iteratively
+        let mut nums = nums;
+        nums.sort_by(|a, b| a.cmp(b));
+        let mut i_nums = Vec::new();
+        let mut j_nums = Vec::new();
+        let mut k_nums = Vec::new();
+        let mut seen_three_sum: HashSet<Vec<i32>> = HashSet::new();
 
-        for i in 0..n {
-            for j in 0..n {
-                for k in 0..n {
-                    if i != j && i != k && j != k {
-                        let sum = nums[i] + nums[j] + nums[k];
-                        if sum == 0 {
-                            three_sums.push(vec![nums[i], nums[j], nums[k]]);
-                        }
+        let mut skipped_k = 0;
+        'i_loop: for i in 0..n {
+            let a = nums[i];
+            let i_seen = i_nums.contains(&a);
+            if i_seen {
+                k_nums.clear();
+                j_nums.clear();
+                continue 'i_loop;
+            }
+            i_nums.push(a);
+            'j_loop: for j in i..n {
+                let b = nums[j];
+                if i == j {
+                    k_nums.clear();
+                    continue 'j_loop;
+                }
+                j_nums.push(b);
+                'k_loop: for k in j..n {
+                    let c = nums[k];
+
+                    let k_seen = k_nums.contains(&c);
+                    if k_seen || (k == j || k == i) {
+                        skipped_k += 1;
+                        continue 'k_loop;
+                    }
+                    k_nums.push(c);
+                    let sum = a + b + c;
+                    if sum == 0 {
+                        let three_sum = vec![a, b, c];
+                        let _ = !seen_three_sum.insert(three_sum);
                     }
                 }
+                k_nums.clear();
             }
+            j_nums.clear();
         }
-
-        three_sums.dedup();
+        three_sums = seen_three_sum.drain().collect();
+        three_sums.sort_by(|a, b| a.cmp(b));
         three_sums
     }
 }
@@ -35,13 +64,39 @@ impl Solution {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::combinatorics::choose;
     #[test]
     fn six_nums_success() {
         let nums = vec![-1, 0, 1, 2, -1, -4];
-        let output: Vec<Vec<i32>> = vec![vec![-1, -1, 2], vec![-1, 0, 1]];
+        let mut output: Vec<Vec<i32>> = vec![vec![-1, -1, 2], vec![-1, 0, 1]];
+        output.sort_by(|a, b| a.cmp(b));
         let res = Solution::three_sum(nums);
 
         assert_eq!(res, output);
     }
+    #[test]
+    fn all_zeros_success() {
+        let nums = vec![0, 0, 0, 0, 0, 0, 0];
+        let mut output: Vec<Vec<i32>> = vec![vec![0, 0, 0]];
+        output.sort_by(|a, b| a.cmp(b));
+        let res = Solution::three_sum(nums);
+
+        assert_eq!(res, output);
+    }
+
+    //#[test]
+    //fn random_nums_pos_and_neg_success() {
+    //    let nums = vec![
+    //        -11, -3, -6, 12, -15, -13, -7, -3, 13, -2, -10, 3, 12, -12, 6, -6, 12, 9, -2, -12, 14,
+    //        11, -4, 11, -8, 8, 0, -12, 4, -5, 10, 8, 7, 11, -3, 7, 5, -3, -11, 3, 11, -13, 14, 8,
+    //        12, 5, -12, 10, -8, -7, 5, -9, -11, -14, 9, -12, 1, -6, -8, -10, 4, 9, 6, -3, -3, -12,
+    //        11, 9, 1, 8, -10, -3, 2, -11, -10, -1, 1, -15, -6, 8, -7, 6, 6, -10, 7, 0, -7, -7, 9,
+    //        -8, -9, -9, -14, 12, -5, -10, -15, -9, -15, -7, 6, -10, 5, -7, -14, 3, 8, 2, 3, 9, -12,
+    //        4, 1, 9, 1, -15, -13, 9, -14, 11, 9,
+    //    ];
+    //    let mut output: Vec<Vec<i32>> = vec![vec![0]];
+    //    output.sort_by(|a, b| a.cmp(b));
+    //    let res = Solution::three_sum(nums);
+
+    //    assert_eq!(res, output);
+    //}
 }
